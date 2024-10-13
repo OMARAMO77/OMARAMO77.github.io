3136d93d6c7f5e0182cea03fab76ed0bda857a83
Omar ACHKIR <omar0642624692@gmail.com>
2024-10-13 09:37:56 -0700

    landing-page.js

--- script/landing-page.js
+++ script/landing-page.js
@@ -2,9 +2,12 @@ const menuLinks = document.querySelectorAll('nav a');
 
 menuLinks.forEach(link => {
   link.addEventListener('click', (event) => {
-    event.preventDefault();
-    const targetSection = document.getElementById(link.hash.slice(1));
-    smoothScrollTo(targetSection);
+    // Skip preventing default for external links
+    if (link.id !== 'getStartedBtn') {
+      event.preventDefault();
+      const targetSection = document.getElementById(link.hash.slice(1));
+      smoothScrollTo(targetSection);
+    }
   });
 });
 
